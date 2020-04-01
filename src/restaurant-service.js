b65e22f877a03e241df36adf92f4172991c07127
Jesse Callahan <jessetylercallahan@gmail.com>
2020-03-31 22:06:49 -0700

    trys to go live

--- src/restaurant-service.js
+++ src/restaurant-service.js
@@ -1,8 +1,7 @@
 export class RestService {
   async getRestByNum(city1) {
     try {
-      let response2 = await fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${city1}&entity_type=city&establishment_type=restaurant&apikey=a2c150d8509102fc9c1aa7084c73cfad
-      API_KEY2 = acd1196ba2169878e1449978dc3cdda4`);
+      let response2 = await fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${city1}&entity_type=city&establishment_type=restaurant&apikey=a2c150d8509102fc9c1aa7084c73cfad`);
       // let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=acd1196ba2169878e1449978dc3cdda4`);
       let jsonifiedResponse;
       if (response2.ok && response2.status == 200) {
