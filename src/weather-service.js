609dc3959d5d70899770d253acc6292f35a18a8b
Jesse Callahan <jessetylercallahan@gmail.com>
2020-03-31 21:54:29 -0700

    trys to go live

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,16 +1,16 @@
 export class WeatherService {
-    async getWeatherByCity(city) {
-        try {
-            let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=acd1196ba2169878e1449978dc3cdda4`);
-            let jsonifiedResponse;
-            if (response.ok && response.status == 200) {
-                jsonifiedResponse = await response.json();
-            } else {
-                jsonifiedResponse = false;
-            }
-            return jsonifiedResponse;
-        } catch {
-            return false;
-        }
+  async getWeatherByCity(city) {
+    try {
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY2}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch {
+      return false;
     }
+  }
 }
