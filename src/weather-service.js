2049379a9337b263c7aea24ae91da990c9a1ebda
Jesse Callahan <jessetylercallahan@gmail.com>
2020-04-03 13:11:56 -0700

    hides api

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,7 +1,7 @@
 export class WeatherService {
   async getWeatherByCity(city) {
     try {
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY2}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
